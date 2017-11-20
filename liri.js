8693adccf04613cdce34f9f584d1bba853c7badd
bongsokkang <bongsokkang@gmail.com>
2017-11-19 23:51:17 -0800

    liri

---
+++ liri.js
@@ -0,0 +1,68 @@
+
+//process.argv
+//require
+//.fs require(fs)
+//switch
+//* `my-tweets`
+//   * `spotify-this-song`
+//   * `movie-this`
+//   * `do-what-it-says`
+
+
+//function movie-this
+//var 
+//for (var key in bandList) {console.log("A " + key + " band is " + bandList[key] + ".");
+
+//checkCode
+//.body(...) request(queryURL,(error, information,body)
+//        console.log(movieName + JSON.parse(body))
+//fs.appendFile("bank.txt", function(error, data){
+//var hello = readText()
+//.body
+//slice
+//append
+//parseJSON
+
+//console.log
+
+//function
+//checkCode
+//.body(...) request(queryURL,(error, information,body)
+//        console.log(movieName + JSON.parse(body))
+//fs.appendFile("bank.txt", function(error, data){
+//var hello = readText()
+//.body
+//slice
+//append
+//parseJSON
+
+//console.log
+
+
+//function
+//checkCode
+//.body(...) request(queryURL,(error, information,body)
+//        console.log(movieName + JSON.parse(body))
+//fs.appendFile("bank.txt", function(error, data){
+//var hello = readText()
+//.body
+//slice
+//append
+//parseJSON
+
+//console.log
+
+
+//function
+//checkCode
+//.body(...) request(queryURL,(error, information,body)
+//        console.log(movieName + JSON.parse(body))
+//fs.appendFile("bank.txt", function(error, data){
+//var hello = readText()
+//.body
+//slice
+//append
+//parseJSON
+
+//console.log
+
